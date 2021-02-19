2da27bbcdc3c340d1166e6c70a1145c50f0373cd
99061142 <99061142@mydavinci.nl>
2021-02-19 10:32:04 +0100

    Bezig met het verkorten van de code

--- index.php
+++ index.php
@@ -6,34 +6,86 @@
   	</head>
   <body>
   	<?php
-    	$vraagEenRequired = $vraagTweeRequired = $vraagDrieRequired = $vraagVierRequired = 
-      $vraagVijfRequired = $vraagZesRequired = $vraagZevenRequired = "";
-    	$vraagEen = $vraagTwee = $vraagDrie = $vraagVier = $vraagVijf = $vraagZes = $vraagZeven = "";
-    ?>
-      <h1>Mad libs</h1>
-    	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-    		<div class="onkundeDiv">
-    		  <p>&emsp;Er heerst paniek...&emsp;&emsp;Onkunde</p>
-        </div>
-    		<h2>Onkunde</h2>
-        Wat zou je graag willen kunnen?<input type="text" name="vraagEen" value="<?php echo $vraagEen;?>">
-        <span>*<?php echo $vraagEenRequired;?></span><br><br>
-        Met welke personen kun je goed opschieten?<input type="text" name="vraagTwee" value="<?php echo $vraagTwee;?>">
-        <span>*<?php echo $vraagTweeRequired;?></span><br><br>  
-        Wat is je favoriete getal?<input type="text" name="vraagDrie" value="<?php echo $vraagDrie;?>">
-        <span>*<?php echo $vraagDrieRequired;?></span><br><br>          	
-        Wat heb je altijd bij je als je op vakantie gaat?<input type="text" name="vraagVier" value="<?php echo $vraagVier;?>">
-        <span>*<?php echo $vraagVierRequired;?></span><br><br>
-        Wat is je beste persoonlijke eigenschap?<input type="text" name="vraagVijf" value="<?php echo $vraagVijf;?>">
-        <span>*<?php echo $vraagVijfRequired;?></span><br><br>
-        Wat is je slechtste persoonlije eigenschap?<input type="text" name="vraagZes" value="<?php echo $vraagZes;?>">
-        <span>*<?php echo $vraagZesRequired;?></span><br><br>
-        Wat is het ergste dat je kan overkomen?<input type="text" name="vraagZeven" value="<?php echo $vraagZeven;?>">
-        <span>*<?php echo $vraagZevenRequired;?></span><br><br>  
-        <input type="submit" name="submit" value="Versturen">
-        <div class="footer">
-    	    <p>Deze website is gemaakt door Spinoza</p>
-    	  </div>
-      </form>        	        	
+      $vraag1Req = $vraag2Req = $vraag3Req = $vraag4Req = $vraag5Req = $vraag6Req = $vraag7Req ="*";
+      $vraag0 = $vraag1 = $vraag3 = $vraag4 = $vraag5 = $vraag6 = $vraag7 = "";
+
+      $vragen = [
+        "Wat zou je graag willen kunnen?",
+        "Met welke personen kun je goed opschieten?",
+        "Wat is je favoriete getal?",
+        "Wat heb je altijd bij je als je op vakantie gaat?",
+        "Wat is je beste persoonlijke eigenschap?",
+        "Wat is je slechtste persoonlijke eigenschap?",
+        "Wat is het ergste dat je kan overkomen?"
+      ];
+
+
+      if($_SERVER["REQUEST_METHOD"] == "POST"){
+        //
+
+        if (empty($_POST["vraag1"])){
+          $vraag1Req = "Name is required";
+        } 
+        //
+        else{
+          $vraag1 = test_input($_POST["vraag1"]);
+          $vraag1Req = "";
+          //
+          if(!preg_match("/^[a-zA-Z-' ]*$/",$vraag1)){
+            $vraag1Req = "Only letters and white space allowed";
+            $vraag1 = "";
+          }
+        }
+        //
+        if (empty($_POST["vraag2"])){
+          $vraag2Req = "Name is required";
+        } 
+        //
+        else{
+          $vraag2 = test_input($_POST["vraag2"]);
+          $vraag2Req = "";
+          //
+          if(!preg_match("/^[a-zA-Z-' ]*$/",$vraag2)){
+            $vraag2Req = "Only letters and white space allowed";
+            $vraag2 = "";
+          }
+        }     
+      }
+
+      //Als de naam en email goed zijn ingevuld gaat het formulier weg
+      if($_POST[""] == ""){?>
+        <h1>Mad libs</h1>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+          <div class="onkundeDiv">
+            <p>&emsp;Er heerst paniek...&emsp;&emsp;Onkunde</p>
+          </div>
+          <h2>Onkunde</h2>
+
+          <?php for($i = 0; $i < 7; $i++){
+            $vraag = 'vraag'.$i; //vraag4
+
+            ?>
+
+            <?php echo $vragen[$i]; ?><input type="text" name="<?php echo "vraag" . $i;?>" value="<?php echo $$vraag ?>">
+            <br>
+          <?php } ?>
+
+
+          <input type="submit" name="submit" value="Versturen">
+          <div class="footer">
+            <p>naam© 2021</p>
+          </div>
+        </form><?php 
+      }
+
+
+      //Haalt bepaalde tekens uit de teksten zodat ze niet meer werken
+      function test_input($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    ?>              	        	
   </body>
 </html>
