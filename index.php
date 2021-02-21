8df42af1bd2645bdd2c989b4fad31a170803402f
99061142 <99061142@mydavinci.nl>
2021-02-21 20:13:43 +0100

    Het eerste formulier werkt

--- index.php
+++ index.php
@@ -29,13 +29,14 @@
 
       // Antwoorden voor de forumulieren
       $antwoorden = array("");
-      for ($arrayPusher = 0; $arrayPusher <= 14; $arrayPusher++){
+      for ($arrayPusher = 0; $arrayPusher <= 5; $arrayPusher++){
         array_push($antwoorden, "");
       }
 
 
+      // Laat op het beeld zien of de vragen goed zijn ingevuld, en wat er fout is gegaan, als het niet goed is ingevuld.
       if($_SERVER["REQUEST_METHOD"] == "POST"){
-        for($i = 0; $i <= 1; $i++){
+        for($i = 0; $i <= 6; $i++){
           if (empty($_POST["vraag$i"])){
             $description[$i] = "<br>De vraag hierboven moet ingevuld worden";
           }
@@ -43,43 +44,46 @@
             $antwoorden[$i] = test_input($_POST["vraag$i"]);
             // Checkt of de naam geen tekens heeft
             if(!preg_match("/^[a-zA-Z-' ]*$/",$antwoorden[$i])){
-              $description[$i] = "<br>Alleen letters en spaties zijn toegestaan";
+              $description[$i] = "<br>De vraag hierboven mag geen tekens bevatten";
               $antwoorden[$i] = "";
             }
           }
         }
       }
+    ?>
 
-?>
 
-      <h1>Mad libs</h1>
-      <div class="onkundeDiv">
-        <p>&emsp;Er heerst paniek...&emsp;&emsp;Onkunde</p>
-      </div>
-      <h2>Onkunde</h2><?php
-      for($i = 0; $i <= 1; $i++){
-        if($antwoorden[$i] == ""){ ?>
-          <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            <?php for($i = 0; $i <= 1; $i++){ ?>
-              <?php echo $vragen[$i]; ?><input type="text" name="<?php echo "vraag$i"?>" value="<?php echo $antwoorden[$i];?>">
-              <span class="description">*<?php echo $description[$i]; ?></span><br><br>
-            <?php } ?>
-            <br>
-            <input class="submit" type="submit" name="submit" value="Versturen">
-          </form>
-        <?php }
-      } ?>
+    <h1>Mad libs</h1>
+    <div class="onkundeDiv">
+      <p>&emsp;Er heerst paniek...&emsp;&emsp;Onkunde</p>
+    </div>
+    <h2>Onkunde</h2><?php
+
+    // Als alle vragen nog niet goed zijn ingevuld blijven de vragen op het scherm staan
+    if(in_array("", $antwoorden)){ ?>
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"><?php
+        // Laat elke vraag op het scherm zien van het formulier "Onkunde"
+        for($i = 0; $i <= 6; $i++){ ?>
+          <?php echo $vragen[$i]; ?><input type="text" name="<?php echo "vraag$i"?>" value="<?php echo $antwoorden[$i];?>">
+          <span class="description">*<?php echo $description[$i]; ?></span><br><br>
+        <?php } ?>
+        <br>
+        <input class="submit" type="submit" name="submit" value="Versturen">
+      </form><?php
+    }
+    // Als alle vragen van het formulier "Onkunde" zijn ingevuld, wordt de teskt in beeld gebracht
+    if(!in_array("", $antwoorden)){ ?>
       <p class="onkundeVerhaal">
-        Test
+        onkunde teskt als de vragen weg zijn
       </p>
-      <div class="footer">
-        <p>naam© 2021</p>
-      </div>
-
+    <?php } ?> 
+    <div class="footer">
+      <p>naam© 2021</p>
+    </div>
 
 
     <?php
-      //Haalt bepaalde tekens uit de teksten zodat ze niet meer werken
+      //Checkt of de vragen zijn ingevuld, en of er geen tekens bevatten.
       function test_input($data){
         $data = trim($data);
         $data = stripslashes($data);
