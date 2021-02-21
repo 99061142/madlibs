c08f8717d25a472d829ffe9512f405088da363cc
99061142 <99061142@mydavinci.nl>
2021-02-21 21:42:56 +0100

    Heb de formulieren afgemaakt, en de verhalen erbij gezet.

---
+++ onkunde.php
@@ -0,0 +1,93 @@
+<!DOCTYPE HTML>
+<html lang="nl">
+	<head>
+    	<link rel="stylesheet" href="styling.css">
+    	<title>B3W2L3</title> 
+  	</head>
+  <body>
+  	<?php
+      // Formulier onkunde vragen
+      $vragenOnkunde = [
+        "Wat zou je graag willen kunnen?",
+        "Met welke personen kun je goed opschieten?",
+        "Wat is je favoriete getal?",
+        "Wat heb je altijd bij je als je op vakantie gaat?",
+        "Wat is je beste persoonlijke eigenschap?",
+        "Wat is je slechtste persoonlijke eigenschap?",
+        "Wat is het ergste dat je kan overkomen?",
+      ];
+
+      // Antwoorden voor de forumulieren
+
+      $antwoordenOnkunde = array("");
+      for ($arrayPusher = 0; $arrayPusher <= 5; $arrayPusher++){
+        array_push($antwoordenOnkunde, "");
+      }
+
+
+      // Laat op het beeld zien of de vragen goed zijn ingevuld, en wat er fout is gegaan, als het niet goed is ingevuld.
+      if($_SERVER["REQUEST_METHOD"] == "POST"){
+        for($i = 0; $i <= 6; $i++){
+          if (empty($_POST["vraag$i"])){
+            $description[$i] = "<br>De vraag hierboven moet ingevuld worden";
+          }
+          else{
+            $antwoordenOnkunde[$i] = test_input($_POST["vraag$i"]);
+            // Checkt of de naam geen tekens heeft
+            if(!preg_match("/^[a-zA-Z-' ]*$/",$antwoordenOnkunde[$i])){
+              $description[$i] = "<br>De vraag hierboven mag geen tekens bevatten";
+              $antwoordenOnkunde[$i] = "";
+            }
+          }
+        }
+      }
+    ?>
+
+
+    <h1>Mad libs</h1>
+    <div class="menu">
+      <ul>
+        <li><a href="#">Er heerst paniek...</a></li>
+        <li><a href="paniek.php">Onkunde</a></li>
+      </ul>
+    </div>
+    <h2>Onkunde</h2><?php
+
+    // Als alle vragen nog niet goed zijn ingevuld blijven de vragen op het scherm staan
+    if(in_array("", $antwoordenOnkunde)){ ?>
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"><?php
+        // Laat elke vraag op het scherm zien van het formulier "Onkunde"
+        for($i = 0; $i <= 6; $i++){ ?>
+          <?php echo $vragenOnkunde[$i]; ?><input type="text" name="<?php echo "vraag$i"?>" value="<?php echo $antwoordenOnkunde[$i];?>">
+          <span class="description">*<?php echo $description[$i]; ?></span><br><br>
+        <?php } ?>
+        <br>
+        <input class="submit" type="submit" name="submit" value="Versturen">
+      </form><?php
+    }
+    // Als alle vragen van het formulier "Onkunde" zijn ingevuld, wordt de teskt in beeld gebracht
+    if(!in_array("", $antwoordenOnkunde)){?>
+      <p class="verhaal">
+        Er zijn veel mensen die niet kunnen <?php echo $antwoordenOnkunde[0]?>. Neem nou <?php echo $antwoordenOnkunde[1]?>. Zelfs met de hulp van een <?php echo $antwoordenOnkunde[3]?> of zelfs <?php echo $antwoordenOnkunde[2]?> 
+        kan <?php echo $antwoordenOnkunde[1]?> niet tekenen. 
+        Dat heeft niets te maken met een gebrek aan <?php echo $antwoordenOnkunde[4]?>, 
+        maar met een te veel aan <?php echo $antwoorden[5]?>. Te veel <?php echo $antwoordenOnkunde[5]?> 
+        leidt tot <?php echo $antwoordenOnkunde[6]?> en dat is niet goed als je wilt <?php echo $antwoordenOnkunde[0]?>. Helaas voor <?php echo $antwoordenOnkunde[1]?>.
+      </p>  
+    <?php } ?> 
+    <div class="footer">
+      <p>naam© 2021</p>
+    </div>
+
+
+    <?php
+      //Checkt of de vragen zijn ingevuld, en of er geen tekens bevatten.
+      function test_input($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    ?>
+  </body>
+</html>
--- paniek.php
+++ paniek.php
@@ -6,17 +6,8 @@
   	</head>
   <body>
   	<?php
-      // Vragen voor de forumulieren
-      $vragen = [
-        // Formulier onkunde vragen
-        "Wat zou je graag willen kunnen?",
-        "Met welke personen kun je goed opschieten?",
-        "Wat is je favoriete getal?",
-        "Wat heb je altijd bij je als je op vakantie gaat?",
-        "Wat is je beste persoonlijke eigenschap?",
-        "Wat is je slechtste persoonlijke eigenschap?",
-        "Wat is het ergste dat je kan overkomen?",
-        // Formulier paniek vragen
+      // Formulier paniek vragen
+      $vragenPaniek = [
         "Welk dier zou je nooit als huisdier willen hebben?",
         "Wie is de belangrijkste persoon in je leven?",
         "In welk land zou je graag willen wonen?",
@@ -28,24 +19,24 @@
       ];
 
       // Antwoorden voor de forumulieren
-      $antwoorden = array("");
-      for ($arrayPusher = 0; $arrayPusher <= 5; $arrayPusher++){
-        array_push($antwoorden, "");
+      $antwoordenPaniek = array("");
+      for ($arrayPusher = 0; $arrayPusher <= 6; $arrayPusher++){
+        array_push($antwoordenPaniek, "");
       }
 
 
       // Laat op het beeld zien of de vragen goed zijn ingevuld, en wat er fout is gegaan, als het niet goed is ingevuld.
       if($_SERVER["REQUEST_METHOD"] == "POST"){
-        for($i = 0; $i <= 6; $i++){
+        for($i = 0; $i <= 7; $i++){
           if (empty($_POST["vraag$i"])){
             $description[$i] = "<br>De vraag hierboven moet ingevuld worden";
           }
           else{
-            $antwoorden[$i] = test_input($_POST["vraag$i"]);
+            $antwoordenPaniek[$i] = test_input($_POST["vraag$i"]);
             // Checkt of de naam geen tekens heeft
-            if(!preg_match("/^[a-zA-Z-' ]*$/",$antwoorden[$i])){
+            if(!preg_match("/^[a-zA-Z-' ]*$/",$antwoordenPaniek[$i])){
               $description[$i] = "<br>De vraag hierboven mag geen tekens bevatten";
-              $antwoorden[$i] = "";
+              $antwoordenPaniek[$i] = "";
             }
           }
         }
@@ -54,17 +45,20 @@
 
 
     <h1>Mad libs</h1>
-    <div class="onkundeDiv">
-      <p>&emsp;Er heerst paniek...&emsp;&emsp;Onkunde</p>
+    <div class="menu">
+      <ul>
+        <li><a href="onkunde.php">Er heerst paniek...</a></li>
+        <li><a href="#">Onkunde</a></li>
+      </ul>
     </div>
-    <h2>Onkunde</h2><?php
+    <h2>Er heerst paniek...</h2><?php
 
     // Als alle vragen nog niet goed zijn ingevuld blijven de vragen op het scherm staan
-    if(in_array("", $antwoorden)){ ?>
+    if(in_array("", $antwoordenPaniek)){ ?>
       <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"><?php
         // Laat elke vraag op het scherm zien van het formulier "Onkunde"
-        for($i = 0; $i <= 6; $i++){ ?>
-          <?php echo $vragen[$i]; ?><input type="text" name="<?php echo "vraag$i"?>" value="<?php echo $antwoorden[$i];?>">
+        for($i = 0; $i <= 7; $i++){ ?>
+          <?php echo $vragenPaniek[$i]; ?><input type="text" name="<?php echo "vraag$i"?>" value="<?php echo $antwoordenPaniek[$i];?>">
           <span class="description">*<?php echo $description[$i]; ?></span><br><br>
         <?php } ?>
         <br>
@@ -72,10 +66,20 @@
       </form><?php
     }
     // Als alle vragen van het formulier "Onkunde" zijn ingevuld, wordt de teskt in beeld gebracht
-    if(!in_array("", $antwoorden)){ ?>
-      <p class="onkundeVerhaal">
-        onkunde teskt als de vragen weg zijn
-      </p>
+    if(!in_array("", $antwoordenPaniek)){ ?>
+      <p class="verhaal">
+        Er heerst paniek in koninkrijk <?php echo $antwoordenPaniek[2]?>. Koning <?php echo $antwoordenPaniek[5]?> is ten einde raad en als koning <?php echo $antwoordenPaniek[5]?> ten einde raad is, dan roept hij zijn ten-einde-raadsheer <?php echo $antwoordenPaniek[1]?>.<br><br>"<?php echo $antwoordenPaniek[1]?>! Het is een ramp! Het is een schande!"
+        <br><br>"Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?"<br><br>"Mijn <?php echo $antwoordenPaniek[0]?> Is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $antwoordenPaniek[4]?> voor hem gekocht!"<br><br>
+        "Majesteit, uw <?php echo $antwoordenPaniek[0]?> komt vast vanzelf weer terug?"<br><br>
+        "Ja da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $antwoordenPaniek[7]?> leren?"<br><br>
+        "Maar Sire, daar kunt u toch uw <?php echo $antwoordenPaniek[6]?> voor gebruiken."<br><br>
+        "<?php echo $antwoordenPaniek[1]?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."<br><br>
+        "<?php echo $antwoordenPaniek[3]?>, Sire."
+
+
+
+
+      </p> 
     <?php } ?> 
     <div class="footer">
       <p>naam© 2021</p>
